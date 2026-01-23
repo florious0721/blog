19d9845c428073bdebc95b13f6f27a98f728bea5
florious <florious@foxmail.com>
2026-01-23 19:39:11 +0800

    feat: 更新题解

--- _ext/oi.py
+++ _ext/oi.py
@@ -56,9 +56,34 @@ class LuoguRole(SphinxRole): # Generate label like `lgp39`
 
         return [tg, ref], []
 
+class VJudgeRole(SphinxRole): # Generate label like `vjhdu-1213`
+    objtype = 'exercise'
+    def run(self) -> tuple[list[nodes.Node], list[nodes.system_message]]:
+        code = self.text
+        label = f'vj{code.lower()}'
+        url = f'https://vjudge.net/problem/{code}'
+        display = f'VJudge {code}'
+
+        nid = make_id(self.env, self.inliner.document, self.objtype, label)
+        tg = nodes.target(
+            '', '', ids=[nid]
+        )
+        ref = nodes.reference(
+            url, display,
+            refuri=url,
+            internal=False
+        )
+        #self.inliner.document.note_explicit_target(tg)
+
+        std = self.env.domains.standard_domain # TODO
+        std.note_hyperlink_target(label, self.env.docname, nid, display)
+
+        return [tg, ref], []
+
 def setup(app: Sphinx):
     app.add_role('leetcode', LeetCodeRole())
     app.add_role('luogu', LuoguRole())
+    app.add_role('vjudge', VJudgeRole())
     return {
         'version': '0.0.3',
         'parallel_read_safe': True,
